2127df6246ab8919c04f7efee59051ef6ee1b897
Rosario <sariodesign.it@gmail.com>
2018-12-02 13:03:42 +0100

    Added images

---
+++ src/assets/javascript/lazy-load.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadThrottleTimeout;
+
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) {
+          document.removeEventListener("scroll", lazyload);
+        }
+    }, 10);
+  }
+
+  document.addEventListener("scroll", lazyload);
+});
