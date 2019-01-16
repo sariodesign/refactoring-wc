4358f201c89be57050111f8bb3727f4ac1b9ed15
Rosario Russo <rosario.russo@blastingnews.com>
2019-01-16 18:29:53 +0100

    Added features on mobile menu

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -1,14 +1,29 @@
 // menu
 const menuBtn = $('.js-nav-mobile');
+const submenuBtn = $('.js-submenu-product');
 
 menuBtn.on('click', function(){
+  $(this).toggleClass('is-open');
   $('.o-overlay').toggleClass('is-visible');
 });
 
-$(document).ready(function(){
-  $('.js-scroll-top').on('click', function(){
-    $('html').animate({
-      scrollTop: 0
-    }, 1000);
-  })
+submenuBtn.on('click', function(){
+  const nextUL = $(this).next('ul');
+
+  if($(this).hasClass('is-active')){
+    nextUL.removeAttr('style').removeClass('is-exploded');
+    $(this).removeClass('is-active');
+  } else {
+    let numberLI = nextUL.children().length;
+    let heightLI = nextUL.children().outerHeight(true);
+    let totalHeightUL = numberLI * heightLI;
+    nextUL.css('height', totalHeightUL+'px').addClass('is-exploded');
+    $(this).addClass('is-active');
+  }
+});
+
+$('.js-scroll-top').on('click', function(){
+  $('html').animate({
+    scrollTop: 0
+  }, 1000);
 });
